8ddc2b831c1e8b29fbf8439e2794aff277c64f99
zero86 <katanazero86@gmail.com>
2020-04-14 01:44:04 +0900

    [feature] react-cart 프로젝트 구성
    - 컴포넌트 반응형 ㅠㅠ
    어렵다..ㅠㅠ

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -17,8 +17,15 @@ function Header() {
         <header className={HeaderStyle.wrap}>
             <div className={HeaderStyle.body}>
                 <div className={HeaderStyle['body-logo']}>
-                    <img src='/assets/images/fco-logo.png'/>
+                    {/*<img src='/assets/images/fco-logo.png'/>*/}
+                    Sweet zero86
                 </div>
+                {/*<nav className={HeaderStyle['body-nav']}>*/}
+                {/*    <ul>*/}
+                {/*        <li>MENU</li>*/}
+                {/*        <li>ABOUT</li>*/}
+                {/*    </ul>*/}
+                {/*</nav>*/}
                 <div className={HeaderStyle['body-user']}>
                     <span className={HeaderStyle['body-user-name']}>zero86 님</span>
                     <img src='/assets/images/shopping-cart-icon.png' onClick={openCartDispatch}/>
