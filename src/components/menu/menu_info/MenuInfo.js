21d67491a50cf067c336abb696266f7b60f71d90
rebch86 <rebch86@daum.net>
2020-04-13 19:11:23 +0900

    [feature] react-cart 프로젝트 기능 구현
    - css 작성

--- src/components/menu/menu_info/MenuInfo.js
+++ src/components/menu/menu_info/MenuInfo.js
@@ -2,20 +2,26 @@ import React from 'react';
 
 import MenuInfoStyle from './MenuInfo.scss';
 
+const currencyFormat = (price) => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
 function MenuInfo({menuName, menuSummary, menuDescription, menuPrice}) {
+
+
     return (
         <div className={MenuInfoStyle.wrap}>
-            <h3>
+            <h3 className={MenuInfoStyle['menu-name']}>
                 {menuName}
             </h3>
-            <p>
+            <p className={MenuInfoStyle['menu-summary']}>
                 {menuSummary}
             </p>
-            <p>
+            <p className={MenuInfoStyle['menu-description']}>
                 {menuDescription}
             </p>
-            <p>
-                {menuPrice}
+            <p className={MenuInfoStyle['menu-price']}>
+                {currencyFormat(menuPrice)}<span>원</span>
             </p>
         </div>
     )
