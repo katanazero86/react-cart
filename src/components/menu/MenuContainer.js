17a935bcc28982d0efcd3fd6f40dd155f9f2f71e
zero86 <katanazero86@gmail.com>
2020-04-14 17:25:54 +0900

    [feature] react-cart 프로젝트 구성
    - webpack 설정 변경 ㅠㅠ
    - image lazy loading 구현

--- src/components/menu/MenuContainer.js
+++ src/components/menu/MenuContainer.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import React, {useEffect, useCallback} from 'react';
+import {useSelector, useDispatch, shallowEqual} from 'react-redux';
 import {getMenus} from '../../store/menuModule/actions'
 
 import MenuContainerStyle from './MenuContainer.scss';
@@ -8,44 +8,16 @@ import Like from "./menu_icon/Like";
 import Cart from "./menu_icon/Cart";
 import MenuInfo from "./menu_info/MenuInfo";
 
-function MenuContainer() {
+const imageLazyLoading = () => {
 
-    const dispatch = useDispatch();
-    const menus = useSelector(store => store.menu.menus);
+    const lazyNodeList = document.querySelectorAll('.img-lazy');
 
-
-    const getMenusDispatch = async () => {
-        dispatch(getMenus());
-    };
-
-    useEffect(() => {
-        getMenusDispatch();
-
-        setTimeout(() => {
-            const lazyNodeList = document.querySelectorAll('.img-lazy');
-            if (lazyNodeList.length > 0) {
-                imageLazyLoading(lazyNodeList);
-            }
-
-        }, 500);
-
-    }, []);
-
-
-    const likeClick = () => {
-        alert('준비중..');
-    };
-
-    const cartClick = () => {
-
-    };
-
-    const imageLazyLoading = (lazyNodeList) => {
+    if(lazyNodeList.length > 0) {
         if (window.IntersectionObserver) {
 
             const options = {
                 root: null,
-                thredhold: 0.6
+                thredhold: 0.8
             }
 
             const observer = new IntersectionObserver((entries, observer) => {
@@ -62,11 +34,43 @@ function MenuContainer() {
             }, options);
             lazyNodeList.forEach(node => observer.observe(node));
 
-
         } else {
-
+            lazyNodeList.forEach(node => {
+                node.setAttribute('src', node.dataset.src);
+                node.dataset.src = '';
+                node.classList.add('fade-in');
+                node.classList.remove('img-lazy');
+            })
         }
+    }
+
+
+};
+
+function MenuContainer() {
+
+    const dispatch = useDispatch();
+    const menus = useSelector(store => store.menu.menus);
+
+    const getMenusDispatch = () => {
+        dispatch(getMenus());
     };
+
+    useEffect(() => {
+        if(menus.length == 0) {
+            getMenusDispatch();
+        }
+        imageLazyLoading();
+    }, [menus.length]);
+
+    const likeClick = () => {
+        alert('준비중..');
+    };
+
+    const cartClick = () => {
+
+    };
+
 
     return (
         <div className={MenuContainerStyle.wrap}>
